a93195c7cd9fd36d0faae56d69e3bc9430b8eedf
agarwalsuyash07-cpu <agarwalsuyash07@gmail.com>
2026-01-09 13:02:22 +0530

    Delete app directory

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import { Redirect } from "expo-router";
-import { useContext } from "react";
-import { AuthContext } from "../src/context/AuthContext";
-
-export default function Index() {
-  const { user } = useContext(AuthContext);
-  return <Redirect href={user ? "/(tabs)/products" : "/login"} />;
-
-}
